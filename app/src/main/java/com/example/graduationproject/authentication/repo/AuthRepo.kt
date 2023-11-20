7e86cf899dde47d6535d43a60b6866242fb64349
Mona Adel <96449268+MonaAdel2@users.noreply.github.com>
2023-11-20 22:46:21 +0200

    Create auth activity, login and sign up fragments, navigation graph, and view model.

---
+++ app/src/main/java/com/example/graduationproject/authentication/repo/AuthRepo.kt
@@ -0,0 +1,4 @@
+package com.example.graduationproject.authentication.repo
+
+class AuthRepo {
+}
---
+++ app/src/main/java/com/example/graduationproject/authentication/viewModel/AuthViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.graduationproject.authentication.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.example.graduationproject.authentication.repo.AuthRepo
+
+class AuthViewModel(val repo: AuthRepo): ViewModel(){
+
+}

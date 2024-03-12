8f62238fd7ddc6a24f8dc031c8cc11ee399281c7
Rodina <Rodinamobark3@gmail.com>
2024-03-12 13:42:27 +0200

    register fragment v1

--- app/src/main/java/com/example/graduationproject/authentication/signup/view/RegisterFragment.kt
+++ app/src/main/java/com/example/graduationproject/authentication/signup/view/RegisterFragment.kt
@@ -8,13 +8,13 @@ import android.view.ViewGroup
 import com.example.graduationproject.R
 
 
-class signupFragment : Fragment() {
+class registerFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
 }

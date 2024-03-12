da55d68284b8c8985ec031c8cfede847066166a3
Rodina <Rodinamobark3@gmail.com>
2024-03-12 05:37:23 +0200

    adding signup fragment

---
+++ app/src/main/java/com/example/graduationproject/authentication/signup/view/signupFragment.kt
@@ -0,0 +1,20 @@
+package com.example.graduationproject.authentication.signup.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.graduationproject.R
+
+
+class signupFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_signup, container, false)
+    }
+
+}

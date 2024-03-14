1cea923c439484f95371138a05de671248a04b19
Rodina <Rodinamobark3@gmail.com>
2024-03-14 16:38:40 +0200

    phone data model

---
+++ app/src/main/java/com/example/graduationproject/authentication/signup/model/PhoneData.kt
@@ -0,0 +1,39 @@
+package com.example.graduationproject.authentication.signup.model
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.google.firebase.auth.PhoneAuthProvider
+
+data class PhoneData(
+    var token: PhoneAuthProvider.ForceResendingToken,
+    var verificationId: String,
+    var phoneNumber: String
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readParcelable(PhoneAuthProvider.ForceResendingToken::class.java.classLoader)!!,
+        parcel.readString().toString(),
+        parcel.readString().toString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(token, flags)
+        parcel.writeString(verificationId)
+        parcel.writeString(phoneNumber)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<PhoneData> {
+        override fun createFromParcel(parcel: Parcel): PhoneData {
+            return PhoneData(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PhoneData?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}

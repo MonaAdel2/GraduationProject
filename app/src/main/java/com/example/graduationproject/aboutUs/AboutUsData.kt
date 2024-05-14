3f1dfc6e6c915d4873ccba8772c3eb6b35bbac3a
Mona Adel <96449268+MonaAdel2@users.noreply.github.com>
2024-05-14 22:54:49 +0300

    Add data to the about us screen.

---
+++ app/src/main/java/com/example/graduationproject/aboutUs/AboutUsData.kt
@@ -0,0 +1,4 @@
+package com.example.graduationproject.aboutUs
+
+data class AboutUsData (var name: String, var photo: Int, var email: String, var linkedin: String){
+}
--- app/src/main/java/com/example/graduationproject/aboutUs/view/AboutUsFragment.kt
+++ app/src/main/java/com/example/graduationproject/aboutUs/view/AboutUsFragment.kt
@@ -5,19 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.graduationproject.R
-
+import com.example.graduationproject.aboutUs.AboutUsData
+import com.example.graduationproject.databinding.FragmentAboutUsBinding
+import com.example.graduationproject.databinding.FragmentHomeBinding
 
 
 class AboutUsFragment : Fragment() {
 
+    private lateinit var  binding :FragmentAboutUsBinding
+
+    private lateinit var aboutUsAdapter: AboutUsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false)
+        binding = FragmentAboutUsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val data = mutableListOf<AboutUsData>()
+            data.add(AboutUsData("Mona Adel Kamal", R.drawable.mona_photo, "monaadel1812@gmail.com", "https://www.linkedin.com/in/mona-adel-808195228/"))
+            data.add(AboutUsData("Rodina Mo'men", R.drawable.rodina_photo, "Rodinamobark3@gmail.com", "https://www.linkedin.com/in/rodina-momen-927b991a5/"))
+
+
+
+        aboutUsAdapter = AboutUsAdapter(data)
+
+        binding.rvAboutUs.layoutManager = LinearLayoutManager(activity)
+        binding.rvAboutUs.adapter = aboutUsAdapter
     }
 
 
